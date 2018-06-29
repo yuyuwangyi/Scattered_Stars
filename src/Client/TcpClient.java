0d0c0d4c889b1c68cb786a14db4a9c468cd2e63c
wy <776723419@qq.com>
2018-06-29 09:21:27 +0800

    commit

---
+++ src/Client/TcpClient.java
@@ -0,0 +1,90 @@
+package Client;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TcpClient {
+    public static void main(String[] args) {
+        try {
+
+            System.out.println("创建客户端，并连接服务端...");
+            InetAddress address = InetAddress.getByName("192.168.11.234");
+            Socket socket = new Socket(address, 12345);
+            ReceiverMsg rm = new ReceiverMsg(socket);
+            SendMsg sm = new SendMsg(socket);
+
+            Thread t1 = new Thread(rm);
+            Thread t2 = new Thread(sm);
+
+            t1.start();
+            t2.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static class ReceiverMsg implements Runnable {
+
+        private Socket socket;
+        private String name;
+
+        public ReceiverMsg(Socket socket) {
+            this.socket = socket;
+            name = socket.getInetAddress().getHostName();
+        }
+
+        @Override
+        public void run() {
+            try {
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                while (true) {
+                    String readLine = br.readLine();
+                    if (readLine == null) {
+                        break;
+                    }
+                    System.out.println(name+":" + readLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    static class SendMsg implements Runnable {
+
+        private Socket socket;
+        private PrintWriter pw;
+
+        public SendMsg(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                @SuppressWarnings("resource")
+                Scanner scanner =new Scanner(System.in);
+
+                pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+                System.out.println("请输入昵称");
+                pw.println(scanner.next());
+                while (true) {
+                    pw.println(scanner.next());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
---
+++ src/Service/TcpService.java
@@ -0,0 +1,77 @@
+package Service;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class TcpService {
+    static HashMap<String, Socket> map=new HashMap<>();
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(30);
+        LinkedList<Socket> list = new LinkedList<>();
+        try {
+            System.out.println("创建服务器");
+            @SuppressWarnings("resource")
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("等待客户端连接");
+            while (true) {
+                Socket accept = serverSocket.accept();
+                System.out.println("客户端已连接");
+                list.add(accept);
+                executor.execute(new Recever(list, accept));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static class Recever implements Runnable {
+        private Socket socket;
+        private LinkedList<Socket> list;
+
+        public Recever(LinkedList<Socket> list, Socket socket) {
+            this.list = list;
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                InputStream input = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(input);
+                BufferedReader br = new BufferedReader(isr);
+                String name = br.readLine();
+                map.put(name, socket);
+                while (true) {
+                    String readLine = br.readLine();
+                    int indexOf = readLine.indexOf("@");
+                    if (indexOf != -1) {
+                        String substring = readLine.substring(indexOf+1, readLine.length());
+                        Set<Map.Entry<String, Socket>> entrySet = map.entrySet();
+                        for (Map.Entry<String, Socket> entry : entrySet) {
+                            if (entry.getKey().equals(substring)) {
+                                PrintWriter pw = new PrintWriter(
+                                        new OutputStreamWriter(entry.getValue().getOutputStream()), true);
+                                pw.println(name + "说:" + readLine.substring(0,indexOf));
+                            }
+                        }
+                    } else {
+                        for (Socket socket1 : list) {
+                            if (socket1 != socket) {
+                                PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket1.getOutputStream()),
+                                        true);
+                                pw.println(name + "说:" + readLine);
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
